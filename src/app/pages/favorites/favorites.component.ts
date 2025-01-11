50673a2697082f6f58be828dcf698d75014643c7
MarkoMandic9 <marko.mandic4444@gmail.com>
2025-01-11 14:46:51 +0100

    Add image grid to favourites page

--- src/app/pages/favorites/favorites.component.ts
+++ src/app/pages/favorites/favorites.component.ts
@@ -1,11 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FavoritesService } from './favorites.service';
+import { NgOptimizedImage } from '@angular/common';
+import { RouterLink } from '@angular/router';
+import { ImageGridComponent } from '../../shared/image-grid/image-grid.component';
 
 @Component({
   selector: 'app-favorites',
-  imports: [],
+  imports: [ImageGridComponent],
   templateUrl: './favorites.component.html',
-  styleUrl: './favorites.component.scss' 
+  styleUrl: './favorites.component.scss',
+  providers: [FavoritesService],
 })
 export class FavoritesComponent {
+  private favoritesService = inject(FavoritesService);
+  readonly images = this.favoritesService.images;
 
+  constructor() {
+    this.favoritesService.loadFavorites();
+  }
 }
--- src/app/shared/image-grid/image-grid.component.ts
+++ src/app/shared/image-grid/image-grid.component.ts
@@ -5,16 +5,17 @@ import { ImageMetadata } from '../interface/image-metadata.interface';
 import { InfiniteScrollDirective } from '../directive/infinite-scroll.directive';
 
 @Component({
-    selector: 'app-image-grid',
-    templateUrl: './image-grid.component.html',
-    imports: [ImageDetailsComponent, CommonModule, InfiniteScrollDirective],
-    styleUrls: ['./image-grid.component.scss']
+  selector: 'app-image-grid',
+  templateUrl: './image-grid.component.html',
+  imports: [ImageDetailsComponent, CommonModule, InfiniteScrollDirective],
+  styleUrls: ['./image-grid.component.scss'],
 })
 export class ImageGridComponent {
-    imagesMetaData = input<ImageMetadata[]>();
-    nextPage = output<void>();
+  imagesMetaData = input<ImageMetadata[]>();
+  nextPage = output<void>();
+  imageClick = output<string>();
 
-    onNextPageLoad(): void {
-        this.nextPage.emit();
-    };
+  onNextPageLoad(): void {
+    this.nextPage.emit();
+  }
 }
