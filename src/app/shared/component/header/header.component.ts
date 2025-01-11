2ad0109f6f0aa73dc0bbed15e0c9ca45c82bb97b
MarkoMandic9 <marko.mandic4444@gmail.com>
2025-01-11 15:59:28 +0100

    Change header

--- src/app/shared/component/header/header.component.ts
+++ src/app/shared/component/header/header.component.ts
@@ -1,14 +1,13 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { MatAnchor, MatButton } from '@angular/material/button';
 import { MatToolbar } from '@angular/material/toolbar';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-header',
-  imports: [MatToolbar, RouterLink, RouterLinkActive],
+  imports: [MatToolbar, RouterLink, RouterLinkActive, MatAnchor],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class HeaderComponent {
-
-}
+export class HeaderComponent {}
