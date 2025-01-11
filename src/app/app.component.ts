e5523482d50680515d72feba2e9222f5672e2980
MarkoMandic9 <marko.mandic4444@gmail.com>
2025-01-11 12:38:54 +0100

    Add header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { MatToolbar } from '@angular/material/toolbar';
+import { MatAnchor } from '@angular/material/button';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RouterLink, RouterLinkActive, MatToolbar, MatAnchor],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
