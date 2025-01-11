3ffdac69d713c5a60701a05d3f5b33c43bd9f2f8
MarkoMandic9 <marko.mandic4444@gmail.com>
2025-01-11 15:54:31 +0100

    Move scroll to document body

--- src/app/shared/components/image-grid/image-grid.component.ts
+++ src/app/shared/components/image-grid/image-grid.component.ts
@@ -1,21 +1,16 @@
-import { Component, input, Input, output } from '@angular/core';
+import { ChangeDetectionStrategy, Component, input, Input, output } from '@angular/core';
 import { ImageGridItemComponent } from '../image-grid-item/image-grid-item.component';
 import { CommonModule } from '@angular/common';
 import { ImageMetadata } from '../../interface/image-metadata.interface';
-import { InfiniteScrollDirective } from '../../directive/infinite-scroll.directive';
 
 @Component({
   selector: 'app-image-grid',
   templateUrl: './image-grid.component.html',
-  imports: [ImageGridItemComponent, CommonModule, InfiniteScrollDirective],
+  imports: [ImageGridItemComponent, CommonModule],
   styleUrls: ['./image-grid.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ImageGridComponent {
   readonly imagesMetaData = input<ImageMetadata[]>();
-  readonly nextPage = output<void>();
   readonly imageClick = output<string>();
-
-  onNextPageLoad(): void {
-    this.nextPage.emit();
-  }
 }
