ed44b66b2bfa37a0a26ef73b94cc48910f23db51
MarkoMandic9 <marko.mandic4444@gmail.com>
2025-01-11 15:08:15 +0100

    Moves to components folder

--- src/app/shared/components/image-grid/image-grid.component.ts
+++ src/app/shared/components/image-grid/image-grid.component.ts
@@ -1,13 +1,13 @@
 import { Component, input, Input, output } from '@angular/core';
-import { ImageDetailsComponent } from '../image-details/image-details.component';
+import { ImageGridItemComponent } from '../image-grid-item/image-grid-item.component';
 import { CommonModule } from '@angular/common';
-import { ImageMetadata } from '../interface/image-metadata.interface';
-import { InfiniteScrollDirective } from '../directive/infinite-scroll.directive';
+import { ImageMetadata } from '../../interface/image-metadata.interface';
+import { InfiniteScrollDirective } from '../../directive/infinite-scroll.directive';
 
 @Component({
   selector: 'app-image-grid',
   templateUrl: './image-grid.component.html',
-  imports: [ImageDetailsComponent, CommonModule, InfiniteScrollDirective],
+  imports: [ImageGridItemComponent, CommonModule, InfiniteScrollDirective],
   styleUrls: ['./image-grid.component.scss'],
 })
 export class ImageGridComponent {
