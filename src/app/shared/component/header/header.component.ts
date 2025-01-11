8407ef89a7e8d2060757aa1976236c4037fafb59
MarkoMandic9 <marko.mandic4444@gmail.com>
2025-01-11 17:12:12 +0100

    Correct header color

--- src/app/shared/component/header/header.component.ts
+++ src/app/shared/component/header/header.component.ts
@@ -1,11 +1,12 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { MatAnchor, MatButton } from '@angular/material/button';
 import { MatIcon } from '@angular/material/icon';
 import { MatToolbar } from '@angular/material/toolbar';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-header',
-  imports: [MatToolbar, RouterLink, RouterLinkActive, MatIcon],
+  imports: [MatToolbar, RouterLink, RouterLinkActive, MatAnchor, MatIcon],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
