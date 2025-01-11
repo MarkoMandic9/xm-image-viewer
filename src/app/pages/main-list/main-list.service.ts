9ea0e7fcdddaabb28caa6827e30e159ca61d88a4
MarkoMandic9 <marko.mandic4444@gmail.com>
2025-01-11 13:58:52 +0100

    Change load start from the first page

--- src/app/pages/main-list/main-list.service.ts
+++ src/app/pages/main-list/main-list.service.ts
@@ -1,8 +1,8 @@
-import { inject, Injectable, Signal } from "@angular/core";
+import { inject, Injectable, signal, Signal } from "@angular/core";
 import { ImageMetadata } from "../../shared/interface/image-metadata.interface";
 import { ImageMetadataService } from "../../shared/service/image-metadata.service";
-import { BehaviorSubject, catchError, concatMap, filter, map, merge, of, scan, share } from "rxjs";
-import { toSignal } from "@angular/core/rxjs-interop";
+import { catchError, map, merge, of, scan, share, Subject, switchMap } from "rxjs";
+import { takeUntilDestroyed, toSignal } from "@angular/core/rxjs-interop";
 import { StorageService } from "../../shared/service/storage.service";
 import { FAVORITES_STORAGE_PATH, IMAGE_PAGE_SIZE } from "../../shared/constants";
 
@@ -11,11 +11,12 @@ export class MainListService {
     private readonly imageMetadataService = inject(ImageMetadataService);
     private readonly storageService = inject(StorageService);
 
-    private readonly pageIndex$ = new BehaviorSubject<number>(-1);
+    private readonly incrementPage$ = new Subject<void>();
 
-    private readonly imagePage$ = this.pageIndex$.pipe(
-        filter((index) => index >= 0),
-        concatMap((index) => this.imageMetadataService.getMetadata(index, IMAGE_PAGE_SIZE)),
+    private readonly pageIndex = signal(1);
+
+    private readonly imagePage$ = this.incrementPage$.pipe(
+        switchMap(() => this.imageMetadataService.getMetadata(this.pageIndex(), IMAGE_PAGE_SIZE)),
         share(),
     );
 
@@ -30,20 +31,24 @@ export class MainListService {
 
     readonly loading = toSignal(
         merge(
-            this.pageIndex$.pipe(map(() => 1)),
-            this.imagePage$.pipe(map(() => -1)),
-        ).pipe(
-            scan((loadingItemCount, increment) => loadingItemCount + increment, 0),
-            map((pageRequests) => pageRequests > 0),
+            this.incrementPage$.pipe(map(() => true)),
+            this.imagePage$.pipe(map(() => false)),
         ), { initialValue: false }
     );
+
+    
+    constructor() {
+        this.imagePage$.pipe(
+            takeUntilDestroyed(),
+        ).subscribe(() => this.pageIndex.update((index) => index + 1));
+    }
 
     private get imageMap(): Map<string, ImageMetadata> {
         return new Map(this.images().map((img) => [img.id, img]));
     }
 
     loadNextPage(): void {
-        this.pageIndex$.next(this.pageIndex$.value + 1);
+        this.incrementPage$.next();
     }
 
     addToFavorites(id: string): boolean {
