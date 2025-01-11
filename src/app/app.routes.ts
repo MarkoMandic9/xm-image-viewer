33cc9471c1d9565f5ef980d4d21407fb164d184b
MarkoMandic9 <marko.mandic4444@gmail.com>
2025-01-11 11:00:31 +0100

    Create basic component structure

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'favorites',
+        loadComponent: () => import('./pages/favorites/favorites.component').then(m => m.FavoritesComponent),
+    },
+    {
+        path: 'photos/{id}',
+        loadComponent: () => import('./pages/photo-view/photo-view.component').then(m => m.PhotoViewComponent),
+    },
+    {
+        path: '',
+        loadComponent: () => import('./pages/main-list/main-list.component').then(m => m.MainListComponent),
+    },
+];
---
+++ src/app/pages/photo-view/photo-view.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-photo-view',
+  imports: [],
+  templateUrl: './photo-view.component.html',
+  styleUrl: './photo-view.component.scss'
+})
+export class PhotoViewComponent {
+
+}
