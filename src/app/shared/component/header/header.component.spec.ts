1ea878af876d45246abdaa46952b94a4ac61c6ab
MarkoMandic9 <marko.mandic4444@gmail.com>
2025-01-12 12:42:08 +0100

    Add header tests

--- src/app/shared/component/header/header.component.spec.ts
+++ src/app/shared/component/header/header.component.spec.ts
@@ -1,7 +1,10 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { RouterTestingModule } from '@angular/router/testing';
+import { By } from '@angular/platform-browser';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
 import { HeaderComponent } from './header.component';
-import { provideRouter } from '@angular/router';
+import { provideRouter, Router } from '@angular/router';
 
 describe('HeaderComponent', () => {
   let component: HeaderComponent;
@@ -9,17 +12,54 @@ describe('HeaderComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [HeaderComponent],
-			providers: [provideRouter([])],
-    })
-    .compileComponents();
+      imports: [
+        HeaderComponent,
+        MatToolbarModule,
+        MatIconModule,
+      ],
+      providers: [
+        provideRouter([
+          { path: '', component: HeaderComponent },
+          { path: 'favorites', component: HeaderComponent },
+        ])
+      ]
+    }).compileComponents();
 
     fixture = TestBed.createComponent(HeaderComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('should create the component', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should render Photos and Favorites navigation items', () => {
+    const navItems = fixture.debugElement.queryAll(By.css('.nav-item'));
+    expect(navItems.length).toBe(2);
+
+    const photosNavItem = navItems[0].query(By.css('span')).nativeElement.textContent.trim();
+    const favoritesNavItem = navItems[1].query(By.css('span')).nativeElement.textContent.trim();
+
+    expect(photosNavItem).toBe('Photos');
+    expect(favoritesNavItem).toBe('Favorites');
+  });
+
+  it('should apply active class to Photos when the route is "/"', async () => {
+    const router = TestBed.inject(Router);
+    await router.navigate(['/']);
+    fixture.detectChanges();
+
+    const activeItem = fixture.debugElement.query(By.css('.active-item.photos'));
+    expect(activeItem).toBeTruthy();
+  });
+
+  it('should apply active class to Favorites when the route is "/favorites"', async () => {
+    const router = TestBed.inject(Router);
+    await router.navigate(['/favorites']);
+    fixture.detectChanges();
+
+    const activeItem = fixture.debugElement.query(By.css('.active-item.favorite'));
+    expect(activeItem).toBeTruthy();
   });
 });
