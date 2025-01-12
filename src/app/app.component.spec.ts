919d88bc6e6dd26a3de26ae2afa9515b6304bf06
MarkoMandic9 <marko.mandic4444@gmail.com>
2025-01-12 12:35:54 +0100

    Fix tests in header and app components

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,12 +14,6 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'xm-image-viewer' title`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('xm-image-viewer');
-  });
-
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
